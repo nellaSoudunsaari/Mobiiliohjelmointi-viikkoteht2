debe3396d6b43f0bf0bb7f18db01ec832ad30ded
Nella <nellasoudunsaari@gmail.com>
2026-01-10 15:26:05 +0200

    init

---
+++ app/src/main/java/com/example/kotlinperusteet/domain/MockData.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinperusteet.domain
+
+val mockTasks = listOf(
+    Task(1, "Kauppalista", "maitoa ja kahvia", 1, "20-01-2026", false),
+    Task(2, "Läksyt", "Matikka kpl 4", 4, "06-01-2026", true),
+    Task(3, "Synttärit", "tee kakku ja monta keksiä", 2, "22-01-2026", false),
+    Task(4, "Leikkaa hiukset", "Varaa aika kampaajalle", 5, "12-01-2026", false),
+    Task(5, "Kalasta", "Kalasta kuha ja ahven", 5, "20-01-3000", false)
+)
